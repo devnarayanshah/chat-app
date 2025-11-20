eba1fcf038ca5c2b6207b849f7cf903e3fb49ee7
devnarayanshah <dev47889@gmail.com>
2025-11-20 19:13:05 +0545

    3

--- frontend/src/hook/useGetMessage.jsx
+++ frontend/src/hook/useGetMessage.jsx
@@ -11,7 +11,7 @@ const useGetMessage = () => {
   useEffect(() => {
     const fetchMessage = async () => {
       try {
-        const res = await axios.get(`${apiurl}apiUrl/api/v1/user/${selectedUser}`, {
+        const res = await axios.get(`${apiurl}apiUrl/api/v1/user/${selectedUser._id}`, {
           withCredentials: true,
         });
 
