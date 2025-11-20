852652cfc6b5ebaa65414405bbd2e776d1f1ff9c
devnarayanshah <dev47889@gmail.com>
2025-11-20 16:50:16 +0545

    credentials

--- frontend/src/hook/useGetOtherUser.jsx
+++ frontend/src/hook/useGetOtherUser.jsx
@@ -1,26 +1,26 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import {useDispatch} from 'react-redux';
-import { setOtherUSers } from '../redux/userSlice';
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setOtherUSers } from "../redux/userSlice";
 
 const useGetOtherUser = () => {
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        const fetchOtherUser = async () => {
-            try {
-                axios.defaults.withCredentials=true;
-                const res= await axios.get('https://chat-app-3eav.onrender.com/api/v1/user/')
-               
-                dispatch(setOtherUSers(res.data))
-                console.log(res.data.message)
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchOtherUser();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchOtherUser = async () => {
+      try {
+        const res = await axios.get(
+          "https://chat-app-3eav.onrender.com/api/v1/user/",
+          { withCredentials: true }
+        );
 
-    },[dispatch])
-  
-}
+        dispatch(setOtherUSers(res.data));
+        console.log(res.data.message);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchOtherUser();
+  }, [dispatch]);
+};
 
-export default useGetOtherUser
+export default useGetOtherUser;
