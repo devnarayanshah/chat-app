3d99a309a7eaf7a810623548e760b92256665c2c
devnarayanshah <dev47889@gmail.com>
2025-11-20 13:51:06 +0545

    nepchat

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['https://chat-app-3eav.onrender.com'],
+        origin: ['https://nepchat.onrender.com'],
         methods: ['GET', 'POST']
     },
 });
